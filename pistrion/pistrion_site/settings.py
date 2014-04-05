c04324c99ab4f04551964bba74c40a14c08227dc
Ben Waters <bsawyerwaters@gmail.com>
2014-04-05 09:21:22 +0000

    more model work it's 5am jesus h christ

--- pistrion/pistrion_site/settings.py
+++ pistrion/pistrion_site/settings.py
@@ -36,6 +36,9 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
+    'voting',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
--- pistrion/voting/models.py
+++ pistrion/voting/models.py
@@ -1,21 +1,42 @@
 from django.db import models
 from model_utils.models import TimeStampedModel
 from django.contrib.auth.models import User
+from localflavor.us.models import PhoneNumberField
 
 # Create your models here.
 class SongPoll(TimeStampedModel):
     creator = models.ForeignKey(User)
-    votes = models.ManyToManyField('Song')
+    votes = models.ManyToManyField('Vote')
 
     def __unicode__(self):
         return '%s' % self.id
 
+
+class Vote(TimeStampedModel):
+    number = PhoneNumberField()
+    vote_id = models.ForeignKey('Song')
+
+    def __unicode__(self):
+        return '%' % self.number
+    
+    
 class Song(TimeStampedModel):
     song_id = models.IntegerField()
+    albumArtist = models.CharField(max_length=255)
+    albumArtistKey = models.CharField(max_length=255)
+    albumKey = models.CharField(max_length=255)
     artist = models.CharField(max_length=255)
-    title = models.CharField(max_length=255)
-    artistKey = models.CharField(max_length=100)
+    artistKey = models.CharField(max_length=255)
+    artistUrl = models.CharField(max_length=255)
+    baseIcon = models.CharField(max_length=255)
     embedUrl = models.CharField(max_length=255)
+    icon = models.CharField(max_length=255)
+    icon400 = models.CharField(max_length=255)
+    key = models.CharField(max_length=25)
+    name = models.CharField(max_length=255)
+    short_url = models.CharField(max_length=255)
+    
+
 
     def __unicode__(self):
         return '%s' % self.title
