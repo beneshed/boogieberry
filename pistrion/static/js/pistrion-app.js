f4d3bf04a36b5819a3c08be79f3c99fcf14b9c8e
Ben Waters <bsawyerwaters@gmail.com>
2014-04-06 01:27:50 +0000

    It's ongit add --all

--- pistrion/static/js/pistrion-app.js
+++ pistrion/static/js/pistrion-app.js
@@ -1,28 +1,35 @@
-var app = angular.module('pistrion-app', []);
+var app = angular.module('pistrion-app', [
+    'ngResource'
+]);
 
 
-app.controller('AppController', function($scope) {
-var nums = []
-while(nums.length < 4){
-    var ranNum=Math.floor((Math.random()*101) + 1);
-    var found=false;
-    for(var i=0; i<nums.length;i++){
-        if(nums[i]==ranNum){found=true;break}
+app.config(function($interpolateProvider) {
+
+    $interpolateProvider.startSymbol("{$");
+    $interpolateProvider.endSymbol("$}");
+    
+});
+
+
+app.controller('AppController', function($scope, $resource, Song) {
+    var nums = [];
+    while(nums.length < 4) {
+        var ranNum=Math.floor((Math.random()*101) + 1);
+        var found=false;
+        for(var i=0; i<nums.length;i++){
+            if(nums[i]==ranNum){found=true;break}
         }
-    if(!found)nums[nums.length]=ranNum;
-}
-    var Song = $resource('/api/songs/:id', {id:'@id'});
-    var song1 = Song.get({id:nums[0]}, function() {
-        song1.$save();
-    });
-    console.log(song1);
-    var song2 = Song.get({id:nums[1]}, function() {
-        song2.$save();
-    });
-    var song3 = Song.get({id:nums[2]}, function() {
-        song3.$save();
-    });
-    var song4 = Song.get({id:nums[3]}, function() {
-        song3.$save();
-    });
+        if(!found)nums[nums.length]=ranNum;
+    }
+    $scope.song1 = Song.get({id:nums[0]});
+    $scope.song2 = Song.get({id:nums[1]});
+    $scope.song3 = Song.get({id:nums[2]});
+    $scope.song4 = Song.get({id:nums[3]});
+    console.log($scope.song1);
+
+    
+});
+
+app.factory('Song', function($resource) {
+    return $resource('/api/songs/:id', {id:'@id'});
 });
